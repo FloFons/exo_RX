f776e61c3b6d4fd9ed679586849f4163a479cf7a
Florent Fonsalas <florent.fonsalas@frichti.co>
2022-03-08 17:42:23 +0100

    Initial Commit

---
+++ exo_RX/exo_RXApp.swift
@@ -0,0 +1,17 @@
+//
+//  exo_RXApp.swift
+//  exo_RX
+//
+//  Created by Florent Fonsalas on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct exo_RXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ exo_RXUITests/exo_RXUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  exo_RXUITestsLaunchTests.swift
+//  exo_RXUITests
+//
+//  Created by Florent Fonsalas on 08/03/2022.
+//
+
+import XCTest
+
+class exo_RXUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
